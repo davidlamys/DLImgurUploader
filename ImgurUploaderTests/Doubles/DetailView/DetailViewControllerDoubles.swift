601a035a7c6144eb5586f91cea08ba8e5c6f846a
David Lam <davidlys@gmail.com>
2021-01-08 23:19:28 +0800

    Rename TWNewsReader to ImgurUploader

---
+++ ImgurUploaderTests/Doubles/DetailView/DetailViewControllerDoubles.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewControllerDoubles.swift
+//  ImgurUploaderTests
+//
+//  Created by David_Lam on 18/5/19.
+//  Copyright © 2019 David_Lam. All rights reserved.
+//
+
+import Foundation
+@testable import ImgurUploader
+
+class DetailViewControllerMock: DetailViewControllerType {
+    var setupViewCalledWithItem: Item!
+
+    func setupView(item: Item) {
+        setupViewCalledWithItem = item
+    }
+
+}
---
+++ ImgurUploaderTests/Doubles/DetailView/DetailViewPresenterDoubles.swift
@@ -0,0 +1,19 @@
+//
+//  DetailViewPresenterDoubles.swift
+//  ImgurUploaderTests
+//
+//  Created by David_Lam on 18/5/19.
+//  Copyright © 2019 David_Lam. All rights reserved.
+//
+
+import Foundation
+@testable import ImgurUploader
+
+class DetailViewPresenterMock: DetailViewPresenterType {
+    weak var view: DetailViewControllerType!
+    var viewDidLoadCalled = false
+
+    func viewDidLoad() {
+        viewDidLoadCalled = true
+    }
+}
